bf2b6419b510b9e09187ab4734e07e28cda1efb5
Liam Prosser <heofonsearch.help@outlook.com>
2026-08-16 13:47:33 -0500

    Update SEARXNG API endpoint URL

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-const SEARXNG_API = "searxng.eshnetwork.space";
+const SEARXNG_API = "https://baresearch.org";
 
 app.use(express.json());
 
