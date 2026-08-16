ff57427da1df1b3d9bc99c119780ab3869f2dd60
Liam Prosser <heofonsearch.help@outlook.com>
2026-08-16 13:29:08 -0500

    Add CORS middleware to server

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,13 @@ const express = require("express");
 
 const app = express();
 
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    next();
+});
+
 const PORT = process.env.PORT || 3000;
 const SEARXNG_API = "https://searx.party";
 
