1b40e44d586d3162334e9c0202ac3c6f38f4b162
Liam Prosser <heofonsearch.help@outlook.com>
2026-08-18 08:54:23 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-const SEARXNG_API = "https://search.unredacted.org";
+const SEARXNG_API = "https://searxng.canine.tools/";
 
 app.use(express.json());
 
