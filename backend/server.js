0735f774fc8c319b55a7f7388cb26da34c89b739
Liam Prosser <heofonsearch.help@outlook.com>
2026-08-18 08:28:53 -0500

    Update SEARXNG API URL in server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ app.use((req, res, next) => {
 });
 
 const PORT = process.env.PORT || 3000;
-const SEARXNG_API = "https://searx.perennialte.ch";
+const SEARXNG_API = "https://searx.unredacted.org";
 
 app.use(express.json());
 
