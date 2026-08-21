fd27912b86f02b6c27986f874a9d6cb5390a3b04
Liam Prosser <heofonsearch.help@outlook.com>
2026-08-21 11:14:25 -0500

    Create da.js

---
+++ languages/da.js
@@ -0,0 +1,16 @@
+const translations = {
+    searchPlaceholder: "Søg på GronnFalk",
+    searchButton: "Søg",
+    web: "Web",
+    images: "Billeder",
+    news: "Nyheder",
+    searching: "Søger med GronnFalk...",
+    enterQuery: "Indtast en søgeforespørgsel.",
+    noResults: "Ingen resultater fundet.",
+    unableToLoad: "Kunne ikke indlæse søgeresultater.",
+    wikipedia: "Wikipedia",
+    readMoreWikipedia: "Læs mere på Wikipedia",
+    fetchedFrom: "Hentet fra",
+    untitledResult: "Resultat uden titel",
+    freeAndOpenSource: "Gratis og open source på GitHub"
+};
