ac06ac23909ac41de8dab92f711ee9eda992babc
Liam Prosser <heofonsearch.help@outlook.com>
2026-08-21 11:11:10 -0500

    Create no.js

---
+++ languages/no.js
@@ -0,0 +1,13 @@
+const translations = {
+    search: "Søk på GronnFalk",
+    searchButton: "Søk",
+    web: "Nett",
+    images: "Bilder",
+    news: "Nyheter",
+    searching: "Søker med GronnFalk...",
+    noResults: "Ingen resultater funnet.",
+    enterQuery: "Skriv inn et søk.",
+    wikipedia: "Wikipedia",
+    readMore: "Les mer på Wikipedia",
+    fetchedFrom: "Hentet fra"
+};
