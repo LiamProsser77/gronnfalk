bdfdf621f2e39691bb67f323a740c5aa68e5f411
Liam Prosser <heofonsearch.help@outlook.com>
2026-08-21 11:12:40 -0500

    Create sv.js

---
+++ languages/sv.js
@@ -0,0 +1,16 @@
+const translations = {
+    searchPlaceholder: "Sök på GronnFalk",
+    searchButton: "Sök",
+    web: "Webb",
+    images: "Bilder",
+    news: "Nyheter",
+    searching: "Söker med GronnFalk...",
+    enterQuery: "Ange en sökfråga.",
+    noResults: "Inga resultat hittades.",
+    unableToLoad: "Det gick inte att ladda sökresultaten.",
+    wikipedia: "Wikipedia",
+    readMoreWikipedia: "Läs mer på Wikipedia",
+    fetchedFrom: "Hämtad från",
+    untitledResult: "Resultat utan titel",
+    freeAndOpenSource: "Gratis och öppen källkod på GitHub"
+};
